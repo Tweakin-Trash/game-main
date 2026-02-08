d0c92a8e4a2030cb841a344e8310471ffb970000
Pranav Mannem <mannp24@students.cs.ubc.ca>
2026-02-07 22:21:28 -0600

    Background for Game

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -17,10 +17,10 @@ export default function TabLayout() {
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
-        name="index"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarStyle: {display: 'none'},
         }}
       />
       <Tabs.Screen
@@ -28,6 +28,7 @@ export default function TabLayout() {
         options={{
           title: 'Explore',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarStyle: {display: 'none'},
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,64 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+// import { ThemedText } from '@/components/themed-text';
+import Entypo from '@expo/vector-icons/Entypo';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet, View } from 'react-native';
 
-export default function HomeScreen() {
+
+export default function GameScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
-
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: 'row',
+        },
+      ]}>
+      <View style={[styles.container, { flex: 1, flexDirection: 'column' }]}>
+        <View style={{ flex: 6, backgroundColor: greenPale }} />
+        <View style={{ flex: 1, backgroundColor: compostGreen, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Entypo name="leaf" size={56} color={greyPale} />
+        </View>
+      </View>
+      <View style={[styles.container, { flex: 1, backgroundColor: greyPale, flexDirection: 'column' }]}>
+        <View style={{ flex: 6, backgroundColor: greyPale }} />
+        <View style={{ flex: 1, backgroundColor: paperGrey, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <FontAwesome name="newspaper-o" size={56} color={greyPale} />
+        </View>
+      </View>
+      <View style={[styles.container, { flex: 1, backgroundColor: bluePale, flexDirection: 'column' }]}>
+        <View style={{ flex: 6, backgroundColor: bluePale }} />
+        <View style={{ flex: 1, backgroundColor: recycleBlue, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <FontAwesome name="recycle" size={56} color={greyPale} />
+        </View>
+      </View>
+      <View style={[styles.container, { flex: 1, backgroundColor: blackPale, flexDirection: 'column' }]}>
+        <View style={{ flex: 6, backgroundColor: blackPale }} />
+        <View style={{ flex: 1, backgroundColor: garbageBlack, display: 'flex', justifyContent: 'center', alignItems: 'center', paddingBottom: 10 }}>
+          <Ionicons name="trash" size={56} color = {greyPale} />
+        </View>
+      </View>
+    </View >
   );
-}
+};
+
+
+const recycleBlue = '#397CE4'
+const compostGreen = '#3C875E';
+const garbageBlack = '#282832';
+const paperGrey = '#5F5F5F';
+
+const bluePale = '#9DE0FF';
+const greenPale = '#A0D7AE';
+const blackPale = '#AAAAB4';
+const greyPale = '#EBEBEB';
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  container: {
+    flex: 1,
+    padding: 0,
+    backgroundColor: 'black',
   },
 });
